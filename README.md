ae38d7acda39b9e23aa1f1fd88f87496d18c80cc
Sean Smith <sean@tgicloud.com>
2015-04-12 21:30:32 -0400

    0.0.4

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tgi-util-make-res
 make .res.* files
+
+ex: bin/tgi-util-make-res source.res assets
---
+++ source.res.js
@@ -0,0 +1,43 @@
+// Autocreated with tgi-util-make-res
+var res = {
+  "_type": "Folder",
+  "Buttons": {
+    "_type": "Folder",
+    "BtnHelp": {
+      "_type": "PNG",
+      "firstFrame": 1,
+      "lastFrame": 2
+    },
+    "BtnMenu": {
+      "_type": "PNG",
+      "firstFrame": 1,
+      "lastFrame": 2
+    },
+    "Play_down": {
+      "_type": "PNG"
+    },
+    "Play_up": {
+      "_type": "PNG"
+    }
+  },
+  "M-GameBG": {
+    "_type": "MP3"
+  },
+  "can_be_nested": {
+    "_type": "Folder",
+    "back": {
+      "_type": "JPG",
+      "firstFrame": 1,
+      "lastFrame": 1
+    },
+    "in_folders": {
+      "_type": "Folder",
+      "face": {
+        "_type": "PNG",
+        "firstFrame": 1,
+        "lastFrame": 54,
+        "zeroPad": 2
+      }
+    }
+  }
+};
